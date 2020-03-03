17f115008f1981fd94738bb444d4879da62cdf5e
Joël Grimberg <github@joelgrimberg.nl>
2020-03-03 13:02:59 +0100

    🐛 FIX BUG: brew installer uses ZSH (non POSIX) function

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if [test ! $(which brew)]; then
   echo "Seting up Homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
