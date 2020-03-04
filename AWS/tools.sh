287b38a0a020bc4f02f6434374eeffcf97216cae
Joël Grimberg <github@joelgrimberg.nl>
2020-03-04 12:03:17 +0100

    🐛 BUG: fix installing awsume_version_4

--- AWS/tools.sh
+++ AWS/tools.sh
@@ -4,5 +4,6 @@ curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 
 echo "installing awsume..."
-pip install awsume
+#pip install awsume
+python-3.6 -m pip install AWSUME
 
